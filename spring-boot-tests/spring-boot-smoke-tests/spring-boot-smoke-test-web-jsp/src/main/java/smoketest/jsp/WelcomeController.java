d3821956a4ab2b6d5fa2752e7f66b34f9dc8947d
itlemon <lemon_jiang@aliyun.com>
2020-09-04 22:58:03 +0800

    【title】 compile spring-boot-repository 2.2.8.RELEASE by itlemon
    description: compile spring-boot-repository 2.2.8.RELEASE by itlemon

--- spring-boot-tests/spring-boot-smoke-tests/spring-boot-smoke-test-web-jsp/src/main/java/smoketest/jsp/WelcomeController.java
+++ spring-boot-tests/spring-boot-smoke-tests/spring-boot-smoke-test-web-jsp/src/main/java/smoketest/jsp/WelcomeController.java
@@ -21,25 +21,24 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WelcomeController {
 
     @Value("${application.message:Hello World}")
-    private String message = "Hello World";
+    private String message;
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @GetMapping("/")
     public String welcome(Map<String, Object> model) {
         model.put("time", new Date());
         model.put("message", this.message);
         return "welcome";
     }
 
-    @RequestMapping("/foo")
-    public String foo(Map<String, Object> model) {
-        throw new RuntimeException("Foo");
+    @GetMapping("/foo")
+    public String foo() {
+        throw new RuntimeException("Error");
     }
 
 }
