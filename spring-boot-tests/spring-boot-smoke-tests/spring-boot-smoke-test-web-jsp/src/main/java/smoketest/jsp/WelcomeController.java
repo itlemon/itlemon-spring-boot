1320bd19abcc04e553c1649b8ab924e1fa378424
itlemon <lemon_jiang@aliyun.com>
2020-09-04 13:57:13 +0800

    【title】 compile spring-boot-repository 2.2.8.RELEASE by itlemon
    description: compile spring-boot-repository 2.2.8.RELEASE by itlemon

--- spring-boot-tests/spring-boot-smoke-tests/spring-boot-smoke-test-web-jsp/src/main/java/smoketest/jsp/WelcomeController.java
+++ spring-boot-tests/spring-boot-smoke-tests/spring-boot-smoke-test-web-jsp/src/main/java/smoketest/jsp/WelcomeController.java
@@ -21,25 +21,25 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class WelcomeController {
 
-	@Value("${application.message:Hello World}")
-	private String message = "Hello World";
+    @Value("${application.message:Hello World}")
+    private String message = "Hello World";
 
-	@GetMapping("/")
-	public String welcome(Map<String, Object> model) {
-		model.put("time", new Date());
-		model.put("message", this.message);
-		return "welcome";
-	}
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String welcome(Map<String, Object> model) {
+        model.put("time", new Date());
+        model.put("message", this.message);
+        return "welcome";
+    }
 
-	@RequestMapping("/foo")
-	public String foo(Map<String, Object> model) {
-		throw new RuntimeException("Foo");
-	}
+    @RequestMapping("/foo")
+    public String foo(Map<String, Object> model) {
+        throw new RuntimeException("Foo");
+    }
 
 }
